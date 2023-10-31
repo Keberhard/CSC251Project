c53bc1669c697c45440b68913e14e40fa06ae7db
Keberhard <keberha1@email.cpcc.edu>
2023-10-30 21:37:49 -0400

    Moved fields and methods to the PolicyHolder Class

--- Policy.java
+++ Policy.java
@@ -3,30 +3,11 @@
 */
 public class Policy
 {
-   private String policyNum;        // The policy number
-   private String provider;      // The provider's name
-   private String firstName;     // The policyholder's first name
-   private String lastName;      // The policyholder's last name
-   private int age;              // The policyholder's age
-   private String smokingStatus;// The policyholder's smoking status smoker or non-smoker
-   private double height;        // The policyholder's height in inches
-   private double weight;        // The policyholder's weight in pounds
+   private String policyNum;          // The policy number
+   private String provider;           // The provider's name
+   private PolicyHolder policyHolder; // 
    
-   /**
-      This constructor sets fields to 0 or null or false.
-   */
-   
-   public Policy()
-   {
-      policyNum = "";
-      provider = "";
-      firstName = "";
-      lastName = "";
-      age = 0;
-      smokingStatus = "";
-      height = 0.0;
-      weight = 0.0;
-   }
+
 
       /**
       This constructor sets all fields to the value passed as an argument.
@@ -40,17 +21,12 @@ public class Policy
       @param weightInLbs The policyholder's weight in pounds.
    */
    
-   public Policy(String policyNumber, String providerName, String fName, String lName,
-   int personAge, String smokerStat, double heightInInches, double weightInLbs)
+   public Policy(String policyNumber, String providerName, PolicyHolder holder)
     {
         policyNum = policyNumber;
         provider = providerName;
-        firstName = fName;
-        lastName = lName;
-        age = personAge;
-        smokingStatus = smokerStat;
-        height = heightInInches;
-        weight = weightInLbs;
+        
+        policyHolder = new PolicyHolder(holder);
     }
 
     /**
@@ -93,127 +69,7 @@ public class Policy
         provider = providerName;
     }
 
-    /**
-        The getFirstName method returns the policyholder's first name.
-        @return The first name of the policyholder.
-     */
-
-    public String getFirstName() 
-    {
-        return firstName;
-    }
-
-    /**
-        The setFirstName method sets the policyholder's first name.
-        @param fName The policyholder's first name.
-     */
-
-    public void setFirstName(String fName) 
-    {
-        firstName = fName;
-    }
-
-    /**
-        The getLastName method returns the policyholder's last name.
-        @return The policyholder's last name.
-     */
-
-    public String getLastName() 
-    {
-        return lastName;
-    }
-
-    /**
-        The setLastName method sets the policyholder's last name.
-        @param lName The policyholder's last name.
-     */
-
-    public void setLastName(String lName) 
-    {
-        lastName = lName;
-    }
-
-    /**
-        The getAge method returns the age of the policholder's age.
-        @return The policyholder's age.
-     */
-
-    public int getAge() 
-    {
-        return age;
-    }
-
-    /**
-        The setAge method sets the age of the policyholer.
-        @param personAge The policyholder's age.
-     */
-
-    public void setAge(int personAge) 
-    {
-        age = personAge;
-    }
-
-    /**
-        The getSmokingStatus method returns smoker or non-smoker.
-        @return Smoker or non-smoker.
-     */
-
-    public String getSmokingStatus() 
-    {
-        return smokingStatus;
-    }
-
-    /**
-        The setSmokingStatus method sets the string to smoker or non-smoker.
-        @param smoking Smoker or non-smoker.
-     */
-
-    public void setSmokingStatus(String smoking) 
-    {
-        smokingStatus = smoking;
-    }
-
-    /**
-        The getHeigth method returns the policyholder's height in inches.
-        @return The policyholder's height in inches.
-     */
-
-    public double getHeight() 
-    {
-        return height;
-    }
     
-
-    /**
-        The setHeight method sets policyholder's height in inches.
-        @param heightInInches The policyholder's height in inches.
-     */
-
-    public void setHeight(double heightInInches) 
-    {
-        height = heightInInches;
-    }
-
-    /**
-        The getWeight method returns the policyholder's weight in pounds.
-        @return The policyholder's weight in pounds.
-     */
-
-    public double getWeight() 
-    {
-        return weight;
-    }
-
-    /**
-        The setWeight method sets the ppolicyholder's weight in pounds.
-        @param weightInLbs The policyholder's weight in pounds.
-     */
-
-    public void setWeight(double weightInLbs) 
-    {
-        weight = weightInLbs;
-    } 
-
     /**
         The calculateBMI methods returns the BMI calculated from the policyholder's weight and height.
         @param weight The policyholder's weight in pounds.
@@ -236,7 +92,7 @@ public class Policy
 
     public double priceOfPolicy()
     {
-        double bmi = calculateBMI(weight, height);
+        double bmi = calculateBMI(policyHolder.getWeight(), policyHolder.getWeight());
         final int BASE_FEE = 600;                       // Insurance policy base fee
         final int SMOKER_FEE = 100;                     // Additional fee if the policyholder is a smoker
         final int OVER_50_FEE = 75;                     // Additional fee if the policyholder is over the age of 50
@@ -244,7 +100,7 @@ public class Policy
         double policyPrice = BASE_FEE;                  // Beginning price of the insurance policy
 
         // Adding additional fee if age is over 50
-        if(age > 50)
+        if(policyHolder.getAge() > 50)
         {  
             policyPrice += OVER_50_FEE;
         }
@@ -256,13 +112,14 @@ public class Policy
         } 
 
         // Adding additional fee if the policyholder is a smoker
-        if(smokingStatus.compareTo("smoker") == 0) 
+        if(policyHolder.getSmokingStatus().equalsIgnoreCase("smoker")) 
         {
             policyPrice += SMOKER_FEE;
         }
 
         return policyPrice;
     }
+
 
 
 
